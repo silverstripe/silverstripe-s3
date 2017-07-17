776417261c568b5091f7a392ce8f95d6ab8ec4d6
Sean Harvey <sean@silverstripe.com>
2017-07-17 15:23:30 +1200

    Support pre-signed requests for CloudFront distribution or S3 URL.

--- src/SilverStripeS3ProtectedAdapter.php
+++ src/SilverStripeS3ProtectedAdapter.php
@@ -2,47 +2,150 @@
 
 namespace Madmatt\SilverStripeS3;
 
+use Aws\CloudFront\CloudFrontClient;
 use Aws\S3\S3Client;
 use League\Flysystem\AwsS3v3\AwsS3Adapter;
 use SilverStripe\Assets\Flysystem\ProtectedAdapter;
-use SilverStripe\Control\Controller;
 
 /**
  * An adapter that allows the use of AWS S3 to store and transmit assets rather than storing them locally.
- *
- * @package Madmatt\SilverStripeS3
  */
 class SilverStripeS3ProtectedAdapter extends AwsS3Adapter implements ProtectedAdapter
 {
     use SilverStripeS3AdapterTrait;
 
-    public function __construct(S3Client $s3Client)
+    /**
+     * Pre-signed request expiration time in seconds.
+     *
+     * @var mixed
+     */
+    protected $expiry = 300;
+
+    /**
+     * @var CloudFrontClient
+     */
+    protected $cloudFrontClient;
+
+    /**
+     * @param S3Client         $s3Client
+     * @param CloudFrontClient $cloudFrontClient
+     */
+    public function __construct(S3Client $s3Client, CloudFrontClient $cloudFrontClient)
     {
+        $this->cloudFrontClient = $cloudFrontClient;
         parent::__construct($s3Client, $this->findAwsBucket(), $this->findBucketPrefix());
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getExpiry()
+    {
+        return $this->expiry;
+    }
+
+    /**
+     * @param mixed $expiry
+     */
+    public function setExpiry($expiry)
+    {
+        $this->expiry = $expires;
+    }
+
+    /**
+     * @return CloudFrontClient
+     */
+    public function getCloudFrontClient()
+    {
+        return $this->cloudFrontClient;
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return string
+     */
+    public function getProtectedUrl($path)
+    {
+        if (
+            getenv('AWS_CLOUDFRONT_PROTECTED_DISTRIBUTION_URL') &&
+            file_exists($this->findCloudFrontPrivateKeyPath())
+        ) {
+            return $this->getDistributionUrl($path);
+        }
+
+        return $this->getBucketUrl($path);
     }
 
     /**
      * @return string
      */
-    public function findBucketPrefix()
+    protected function findBucketPrefix()
     {
         $prefix = 'protected';
         if (getenv('AWS_PROTECTED_BUCKET_PREFIX') !== false) {
             $prefix = (string) getenv('AWS_PROTECTED_BUCKET_PREFIX');
         }
+
         return $prefix;
     }
 
     /**
-     * Used by SilverStripe to get the protected URL for this file. This utilises the default ProtectedFileController
-     * class to read the file from AWS (rather than linking to AWS directly).
+     * @throws LogicException
      *
-     * @param string $path
      * @return string
      */
-    public function getProtectedUrl($path)
+    protected function findCloudFrontPrivateKeyId()
     {
-        // Utilise the default SilverStripe\Assets\Storage\ProtectedFileController to shield access
-        return Controller::join_links(ASSETS_DIR, $path);
+        if (getenv('AWS_CLOUDFRONT_PRIVATE_KEY_ID') !== false) {
+            return (string) getenv('AWS_CLOUDFRONT_PRIVATE_KEY_ID');
+        }
+        throw new LogicException('AWS_CLOUDFRONT_PRIVATE_KEY_ID environment variable not set');
+    }
+
+    /**
+     * @throws LogicException
+     *
+     * @return string
+     */
+    protected function findCloudFrontPrivateKeyPath()
+    {
+        if (getenv('AWS_CLOUDFRONT_PRIVATE_KEY_PATH') !== false) {
+            return (string) getenv('AWS_CLOUDFRONT_PRIVATE_KEY_PATH');
+        }
+        throw new LogicException('AWS_CLOUDFRONT_PRIVATE_KEY_PATH environment variable not set');
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return string
+     */
+    protected function getBucketUrl($path)
+    {
+        $cmd = $this->getClient()
+            ->getCommand('GetObject', [
+                'Bucket' => $this->getBucket(),
+                'Key' => $this->applyPathPrefix($path),
+            ]);
+
+        return (string) $this->getClient()
+            ->createPresignedRequest($cmd, time() + $this->getExpiry())
+            ->getUri();
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return string
+     */
+    protected function getDistributionUrl($path)
+    {
+        return $this->getCloudFrontClient()->getSignedUrl([
+            'url' => sprintf('%s/%s', getenv('AWS_CLOUDFRONT_PROTECTED_DISTRIBUTION_URL'), $path),
+            'expires' => time() + $this->getExpiry(),
+            'private_key' => $this->findCloudFrontPrivateKeyPath(),
+            'key_pair_id' => $this->findCloudFrontPrivateKeyId(),
+        ]);
     }
 }
--- src/SilverStripeS3PublicAdapter.php
+++ src/SilverStripeS3PublicAdapter.php
@@ -2,6 +2,7 @@
 
 namespace Madmatt\SilverStripeS3;
 
+use Aws\CloudFront\CloudFrontClient;
 use Aws\S3\S3Client;
 use League\Flysystem\AwsS3v3\AwsS3Adapter;
 use SilverStripe\Assets\Flysystem\PublicAdapter;
@@ -10,32 +11,73 @@ class SilverStripeS3PublicAdapter extends AwsS3Adapter implements PublicAdapter
 {
     use SilverStripeS3AdapterTrait;
 
-    public function __construct(S3Client $s3Client)
+    /**
+     * @var CloudFrontClient
+     */
+    protected $cloudFrontClient;
+
+    /**
+     * @param S3Client         $s3Client
+     * @param CloudFrontClient $cloudFrontClient
+     */
+    public function __construct(S3Client $s3Client, CloudFrontClient $cloudFrontClient)
     {
+        $this->cloudFrontClient = $cloudFrontClient;
         parent::__construct($s3Client, $this->findAwsBucket(), $this->findBucketPrefix());
+    }
+
+    /**
+     * @return CloudFrontClient
+     */
+    public function getCloudFrontClient()
+    {
+        return $this->cloudFrontClient;
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return string
+     */
+    public function getPublicUrl($path)
+    {
+        if (getenv('AWS_CLOUDFRONT_PUBLIC_DISTRIBUTION_URL')) {
+            return $this->getDistributionUrl($path);
+        }
+
+        return $this->getBucketUrl($path);
     }
 
     /**
      * @return string
      */
-    public function findBucketPrefix()
+    protected function findBucketPrefix()
     {
         $prefix = 'public';
         if (getenv('AWS_PUBLIC_BUCKET_PREFIX') !== false) {
             $prefix = (string) getenv('AWS_PUBLIC_BUCKET_PREFIX');
         }
+
         return $prefix;
     }
 
     /**
-     * Used to get the public URL to a file in an S3 bucket. The standard S3 URL is returned (the file is not proxied
-     * further via SilverStripe).
-     *
      * @param string $path
+     *
      * @return string
      */
-    public function getPublicUrl($path)
+    protected function getBucketUrl($path)
     {
         return $this->getClient()->getObjectUrl($this->getBucket(), $this->applyPathPrefix($path));
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return string
+     */
+    protected function getDistributionUrl($path)
+    {
+        return sprintf('%s/%s', getenv('AWS_CLOUDFRONT_PUBLIC_DISTRIBUTION_URL'), $path);
     }
 }
