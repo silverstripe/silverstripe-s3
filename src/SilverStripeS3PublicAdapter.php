c0f5762f421309c042b21bf1b1f580461449abe4
Sean Harvey <sean@silverstripe.com>
2017-07-18 14:30:54 +1200

    Ditch CloudFront for now, this will come later.

--- src/SilverStripeS3PublicAdapter.php
+++ src/SilverStripeS3PublicAdapter.php
@@ -2,7 +2,6 @@
 
 namespace Madmatt\SilverStripeS3;
 
-use Aws\CloudFront\CloudFrontClient;
 use Aws\S3\S3Client;
 use League\Flysystem\AwsS3v3\AwsS3Adapter;
 use SilverStripe\Assets\Flysystem\PublicAdapter;
@@ -12,26 +11,11 @@ class SilverStripeS3PublicAdapter extends AwsS3Adapter implements PublicAdapter
     use SilverStripeS3AdapterTrait;
 
     /**
-     * @var CloudFrontClient
+     * @param S3Client $s3Client
      */
-    protected $cloudFrontClient;
-
-    /**
-     * @param S3Client         $s3Client
-     * @param CloudFrontClient $cloudFrontClient
-     */
-    public function __construct(S3Client $s3Client, CloudFrontClient $cloudFrontClient)
+    public function __construct(S3Client $s3Client)
     {
-        $this->cloudFrontClient = $cloudFrontClient;
         parent::__construct($s3Client, $this->findAwsBucket(), $this->findBucketPrefix());
-    }
-
-    /**
-     * @return CloudFrontClient
-     */
-    public function getCloudFrontClient()
-    {
-        return $this->cloudFrontClient;
     }
 
     /**
@@ -41,11 +25,7 @@ class SilverStripeS3PublicAdapter extends AwsS3Adapter implements PublicAdapter
      */
     public function getPublicUrl($path)
     {
-        if (getenv('AWS_CLOUDFRONT_PUBLIC_DISTRIBUTION_URL')) {
-            return $this->getDistributionUrl($path);
-        }
-
-        return $this->getBucketUrl($path);
+        return $this->getClient()->getObjectUrl($this->getBucket(), $this->applyPathPrefix($path));
     }
 
     /**
@@ -59,25 +39,5 @@ class SilverStripeS3PublicAdapter extends AwsS3Adapter implements PublicAdapter
         }
 
         return $prefix;
-    }
-
-    /**
-     * @param string $path
-     *
-     * @return string
-     */
-    protected function getBucketUrl($path)
-    {
-        return $this->getClient()->getObjectUrl($this->getBucket(), $this->applyPathPrefix($path));
-    }
-
-    /**
-     * @param string $path
-     *
-     * @return string
-     */
-    protected function getDistributionUrl($path)
-    {
-        return sprintf('%s/%s', getenv('AWS_CLOUDFRONT_PUBLIC_DISTRIBUTION_URL'), $path);
     }
 }
