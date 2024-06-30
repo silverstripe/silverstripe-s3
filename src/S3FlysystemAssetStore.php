0da49bfbe3ab66eeb64832b8db1c2789a821ef8b
Will Rossiter <will.rossiter@akqa.com>
2024-07-01 10:25:49 +1200

    feat: implement `check_legacy_path` for hashed public file paths

--- src/S3FlysystemAssetStore.php
+++ src/S3FlysystemAssetStore.php
@@ -14,9 +14,29 @@ class S3FlysystemAssetStore extends BaseFlysystemAssetStore
     /**
      * @config
      *
+     * In pre 4.4 versions of SilverStripe, public files were stored in a
+     * hashed folder structure such as public/{folder}/{hash}/{filename}. By
+     * default we have moved away from that structure to a more simple structure
+     * but if you have files that were uploaded in a pre 4.4 version then enable
+     * this option.
+     *
+     * @var bool
+     */
+    private static $check_legacy_path = false;
+
+    /**
+     * @config
+     *
      * @var bool
      */
     private static $throw_exceptions_on_missing_files = true;
+
+    /**
+     * @var bool
+     */
+    private $checkedForLegacyPath = false;
+
+
 
     protected function applyToFileOnFilesystem(callable $callable, ParsedFileID $parsedFileID, $strictHashCheck = true)
     {
@@ -82,7 +102,7 @@ class S3FlysystemAssetStore extends BaseFlysystemAssetStore
         }
 
         $result = $this->applyToFileOnFilesystem(
-            function (ParsedFileID $parsedFileID, Filesystem $fs, FileResolutionStrategy $strategy) use ($filename) {
+            function (ParsedFileID $parsedFileID, Filesystem $fs, FileResolutionStrategy $strategy) {
                 $parsedFileID = $strategy->stripVariant($parsedFileID);
 
                 if ($parsedFileID && $originalFileID = $parsedFileID->getFileID()) {
@@ -95,6 +115,26 @@ class S3FlysystemAssetStore extends BaseFlysystemAssetStore
             },
             new ParsedFileID($filename, $hash, $variant)
         ) ?: false;
+
+        // if the file is not found then it may be a legacy file where even
+        // public files are stored hashed (i.e public/{folder}/{hash}/{filename})
+        if (!$result && !$this->checkedForLegacyPath && $this->config()->check_legacy_path) {
+            $exploded = explode('/', $filename);
+            $newPath = [];
+
+            $this->checkedForLegacyPath = true;
+
+            foreach ($exploded as $i => $part) {
+                if ($i === count($exploded) - 1) {
+                    $newPath[] = substr($hash, 0, 10);
+                }
+
+                $newPath[] = $part;
+            }
+
+            $result = $this->exists(implode('/', $newPath), $hash, $variant);
+            $this->checkedForLegacyPath = false;
+        }
 
         return $result;
     }
